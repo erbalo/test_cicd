6b8a8631195fca3177d2d6c9ae1d7928ca039fa1
Erick Barrera <dracil.erbalo@gmail.com>
2021-10-12 17:50:51 -0500

    Dolera????

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - Ay mama me esta doliendo
+          Learn React - Ay mama me esta doliendo otro commit
         </a>
       </header>
     </div>
